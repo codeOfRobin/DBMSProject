9880841d953e4e8a499545570ea235190e656753
Robin Malhotra <malhotra.robin@icloud.com>
2016-04-19 00:30:34 +0530

    fixed. I iz idiott

--- app.js
+++ app.js
@@ -3,9 +3,12 @@ var app = express();
 var morgan = require('morgan');
 var Potluck = require('./models/potluck')
 var async = require('async')
+var bodyParser = require('body-parser');
 
 app.use(express.static(__dirname + '/public'));
 app.use(morgan('tiny'))
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 // Potluck.findAll().then(function(potlucks){
 //     console.log(potlucks.map(function(p){
 //         return p.dataValues
@@ -13,11 +16,11 @@ app.use(morgan('tiny'))
 // })
 
 app.get('*',function(req,res){
-    res.sendfile('./public/index.html');
+    res.sendFile(__dirname + '/public/index.html');
 })
 
 app.post('/songs',function(req,res){
-    res.json(["forgot me now", "heartless", "Bury it"])
+	res.json(req.body);
 })
 app.listen(4000);
-console.log('Magic happens on port 3000');
+console.log('Magic happens on port 4000');
--- public/core.js
+++ public/core.js
@@ -4,13 +4,14 @@ var musicApp = angular.module('musicApp', ['ngFileUpload']);
 musicApp.controller('mainController',function($scope, $http) {
     $scope.formData = {};
 
-    // when landing on the page, get all todos and show them
-    $http.post('/songs')
-        .success(function(data) {
-            $scope.songs = data;
-            console.log(data);
-        })
-        .error(function(data) {
-            console.log('Error: ' + data);
-        });
+    var data = {};
+    data.title = "title";
+    data.message = "message";
+    data.songs = ["forgot me now", "heartless", "Bury it"]
+    $.post( "/songs", data)
+    .done(function( data ) {
+        console.log(data);
+        $scope.songs = data.songs
+        $scope.$apply();
+    });
 })
