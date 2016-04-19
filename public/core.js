fb1a0c504c7bc726b55bbbdf085381b7a6937e61
Robin Malhotra <malhotra.robin@yahoo.co.in>
2016-04-19 19:48:34 +0530

    dsafasdf

--- public/core.js
+++ public/core.js
@@ -1,7 +1,35 @@
 var musicApp = angular.module('musicApp', ['ngFileUpload']);
 
+musicApp.controller('mainController', ['$scope', 'Upload', function ($scope, Upload) {
+    // upload later on form submit or something similar
+    $scope.submit = function() {
+        if ($scope.form.file.$valid && $scope.file) {
+            $scope.upload($scope.file);
+        }
+    };
 
-musicApp.controller('mainController',function($scope, $http) {
+    // upload on file select or drop
+    $scope.upload = function (file) {
+        Upload.upload({
+            url: 'upload/url',
+            data: {file: file, 'username': $scope.username}
+        }).then(function (resp) {
+            console.log('Success ' + resp.config.data.file.name + 'uploaded. Response: ' + resp.data);
+        }, function (resp) {
+            console.log('Error status: ' + resp.status);
+        }, function (evt) {
+            var progressPercentage = parseInt(100.0 * evt.loaded / evt.total);
+            console.log('progress: ' + progressPercentage + '% ' + evt.config.data.file.name);
+        });
+    };
+    // for multiple files:
+    $scope.uploadFiles = function (files) {
+        if (files && files.length)
+        {
+            // or send them all together for HTML5 browsers:
+            Upload.upload({data: {file: files}});
+        }
+    }
     $scope.formData = {};
 
     var data = {};
@@ -14,4 +42,4 @@ musicApp.controller('mainController',function($scope, $http) {
         $scope.songs = data.songs
         $scope.$apply();
     });
-})
+}]);
