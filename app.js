a3d0a0500d9fedb07b1da1347e2c9744e3124fc8
Robin Malhotra <malhotra.robin@icloud.com>
2016-04-20 19:12:09 +0530

    fb done. upload done

--- app.js
+++ app.js
@@ -2,38 +2,107 @@ var express = require('express');
 var app = express();
 var morgan = require('morgan');
 var Potluck = require('./models/potluck')
+var User = require('./models/user')
 var async = require('async')
 var bodyParser = require('body-parser');
 var multer  = require('multer')
 var upload = multer({ dest: './uploads' })
-
+var passport = require('passport')
+var FacebookStrategy = require('passport-facebook').Strategy;
+var fs = require('fs')
+User.sync({force: true})
 app.use(express.static(__dirname + '/public'));
 app.use(morgan('tiny'))
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(passport.initialize());
+app.use(passport.session());
 // Potluck.findAll().then(function(potlucks){
 //     console.log(potlucks.map(function(p){
 //         return p.dataValues
 //     }));
 // })
 
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(user, done) {
+  done(null, user);
+});
+passport.use(new FacebookStrategy({
+
+    // pull in our app id and secret from our auth.js file
+    clientID        : "473466989524267",
+    clientSecret    : "e6cf1704ddfca7af4c590dc656d50377",
+    callbackURL     : "http://localhost:4000/auth/facebook/callback",
+    profileFields: ['id', 'displayName', 'photos', 'email']
+},
+function(token, refreshToken, profile, done)
+{
+    process.nextTick(function()
+    {
+        User.findOne({ where: {fbID: profile.id} }).then(function(user)
+        {
+            if (user)
+            {
+                return done(null, user); // user found, return that user
+            }
+            else
+            {
+                var user = User.build({
+                    name: profile.displayName,
+                    fbID: profile.id,
+                    email: profile.emails[0].value,
+                    fbToken: token
+                })
+                user.save().then(function() {
+                    return done(null, user);
+                })
+
+            }
+        })
+    });
+
+}));
+
+app.get('/auth/facebook', passport.authenticate('facebook', { scope : ['user_friends', 'email']}));
+
+app.get('/auth/facebook/callback',
+        passport.authenticate('facebook', {
+            successRedirect : '/',
+            failureRedirect : '/'
+        }));
+
+
+app.get('/*.mp3',function(req,res)
+{
+    console.log(req.params);
+    res.sendFile(__dirname + '/uploads/'+req.params[0]+'.mp3');
+})
 app.get('*',function(req,res){
     res.sendFile(__dirname + '/public/index.html');
 })
 
 app.post('/songs',function(req,res){
-	res.json(req.body);
+    res.json(req.body);
 })
 
 app.post('/uploadSongs', upload.any(), function (req, res, next) {
-  // req.files is an object (String -> Array) where fieldname is the key, and the value is array of files
-  //
-  // e.g.
-  //  req.files['avatar'][0] -> File
-  //  req.files['gallery'] -> Array
-  //
-  // req.body will contain the text fields, if there were any
-  console.log(req.body);
+    tmp_path = req.files[0].path;
+    originalName=req.files[0].originalname;
+    target_path =  req.files[0].path +'.' + "mp3"
+    fs.rename(tmp_path, target_path, function(err) {
+        if (err)
+        throw err;
+        // Delete the temporary file, so that the explicitly set temporary upload dir does not get filled with unwanted files.
+        fs.unlink(tmp_path, function() {
+            if (err)
+            throw err;
+            //
+            res.json("done : "+ target_path)
+        });
+    });
 })
 app.listen(4000);
 console.log('Magic happens on port 4000');
---
+++ models/User.js
@@ -0,0 +1,20 @@
+var db = require('./db');
+var Sequelize = require('sequelize')
+
+var User = db.define('user', {
+    id:  {
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV1,
+        primaryKey: true
+    },
+    name: Sequelize.TEXT,
+    fbID: Sequelize.TEXT,
+    fbToken : Sequelize.TEXT,
+    email : Sequelize.TEXT
+},{
+    timestamps: false,
+    freezeTableName: true,
+    tableName: 'user'
+})
+
+module.exports = User;
