6cb0e99ca28199090978c82ba15d79ecd27bc772
Robin Malhotra <malhotra.robin@icloud.com>
2016-04-23 16:50:44 +0530

    added rating

---
+++ models/rating.js
@@ -0,0 +1,14 @@
+var db = require('./db');
+var Sequelize = require('sequelize')
+
+var Rating = db.define('rating', {
+    userId: Sequelize.UUID,
+    songId: Sequelize.UUID,
+    rating: Sequelize.INTEGER
+},{
+    timestamps: false,
+    freezeTableName: true,
+    tableName: 'rating'
+})
+
+module.exports = Song;
